0b2c9fc7276da29777d3764e1d562c7d86aaa36b
marik <marikdorito@gmail.com>
2023-10-08 23:45:18 +0300

    absolute fix 2

--- Gui.py
+++ Gui.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import ttk
 import random
 
-c, count = 0, 0
+co, count = 0, 0
 x = 'user.txt'
 
 root = Tk()
@@ -14,50 +14,53 @@ def win():
     return True
 
 
-def start_game(c):
+def start_game(co):
     try:
-        if c != 0:
+        if co != 0:
             return False
-        window1 = Toplevel()
-        window1.title("Сначала набери 5 очков!")
-        c = Canvas(window1, width=500, height=500, bg="white")
-        p = Canvas(window1, width=500, height=100, bg="blue")
-        lbl = ttk.Label(window1, text="КЛИКАЙ НА ШАРИКИ!", font="Arial 30", anchor="s")
-        c.pack()
-        p.pack()
-        lbl.pack()
-        window1.grab_set()
-        colors = ["red", "blue", "green", "yellow", "orange"]
+        else:
+            window1 = Toplevel()
+            window1.title("Сначала набери 5 очков!")
+            c = Canvas(window1, width=500, height=500, bg="white")
+            p = Canvas(window1, width=500, height=100, bg="blue")
+            lbl = ttk.Label(window1, text="КЛИКАЙ НА ШАРИКИ!", font="Arial 30", anchor="s")
+            c.pack()
+            p.pack()
+            lbl.pack()
+            window1.grab_set()
+            colors = ["red", "blue", "green", "yellow", "orange"]
 
-        def ball():
-            c.delete(ALL)
-            x = random.randint(20, 480)
-            y = random.randint(20, 480)
-            r = 30
-            new_ball = c.create_oval(x - r, y - r, x + r, y + r, fill=random.choice(colors), width=0)
-            c.tag_bind(new_ball, "<Button-1>", click_ball)
-            window1.after(1000, ball)
+            def ball():
+                c.delete(ALL)
+                x = random.randint(20, 480)
+                y = random.randint(20, 480)
+                r = 30
+                new_ball = c.create_oval(x - r, y - r, x + r, y + r, fill=random.choice(colors), width=0)
+                c.tag_bind(new_ball, "<Button-1>", click_ball)
+                window1.after(1000, ball)
 
-        def click_ball(event):
-            global count
-            count += 1
-            p.delete(ALL)
-            p.create_text(80, 30, font="Arial 20", text="Points:")
-            p.create_text(180, 30, font="Arial 22", text=str(count))
-            if count >= 5:
-                window1.destroy()
+            def click_ball(event):
+                global count
+                count += 1
+                p.delete(ALL)
+                p.create_text(80, 30, font="Arial 20", text="Points:")
+                p.create_text(180, 30, font="Arial 22", text=str(count))
+                if count >= 5:
+                    window1.destroy()
 
-        ddd = win()
-        ball()
-        btn.config(state="normal")
-        print(ddd)
-        return ddd
-    except Exception as e:
-        return False
+            ddd = win()
+            ball()
+            btn.config(state="normal")
+            print(ddd)
+            return ddd
+    except TclError as e:
+        return True
 
 
 def PasswordsDisplay(x):
     try:
+        if x != 'user.txt':
+            return False
         editor = Text()
         editor.pack(fill=BOTH, expand=1)
         with open(x, "r") as file:
@@ -90,7 +93,7 @@ def PasswordsDisplay(x):
 
 btn = ttk.Button(text="Показать пароли", command=lambda: PasswordsDisplay(x), state="disabled")
 btn.pack()  # размещение в окне
-btn1 = ttk.Button(text="Выполнить задание", command=lambda: start_game(c))
+btn1 = ttk.Button(text="Выполнить задание", command=lambda: start_game(co))
 btn1.pack()
 
 root.mainloop()
--- TestsP.py
+++ TestsP.py
@@ -5,11 +5,12 @@ from Gui import *
 def test_start_game():
     assert start_game(5) == False
     assert start_game(4) == False
-    assert start_game(0) == False
+    assert start_game(0) == True
     assert start_game(None) is False
 
 
 def test_PasswordsDisplay():
     assert PasswordsDisplay('user.txt') == 'aaa'
+    assert PasswordsDisplay('user1.txt') == False
     assert PasswordsDisplay('') == False
-    assert PasswordsDisplay(None) == None
+    assert PasswordsDisplay(None) == False
