d6ceae02722c2ac365fa7896a7882d732d69e617
marik <marikdorito@gmail.com>
2023-10-08 22:13:26 +0300

    RENAME WRONG FILES

---
+++ TestsP.py
@@ -0,0 +1,34 @@
+import pytest
+
+def start_game(count: int):
+    try:
+        if count >= 5:
+            return True
+        else:
+            return False
+    except Exception as e:
+        print("you losed")
+        return None
+
+def PasswordsDisplay(x : str):
+    try:
+        if x == '':
+            return False
+        else:
+            with open(x, "r") as file:
+                text = file.read()
+            return text
+    except Exception as e:
+        return None
+
+
+def test_start_game():
+    assert start_game(5) == True
+    assert start_game(4) == False
+    assert start_game(0) == None
+    assert start_game(None) == None
+
+def test_PasswordsDisplay():
+    assert PasswordsDisplay('user.txt') == 'aaa'
+    assert PasswordsDisplay('') == False
+    assert PasswordsDisplay(None) == None
